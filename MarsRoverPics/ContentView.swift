3dd806881246e47645af193833cc601c833a1f86
mizech <michael.zech@hotmail.de>
2024-06-22 09:11:00 +0200

    Initial Commit

---
+++ MarsRoverPics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarsRoverPics
+//
+//  Created by Michael on 22.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarsRoverPics/MarsRoverPicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarsRoverPicsApp.swift
+//  MarsRoverPics
+//
+//  Created by Michael on 22.06.24.
+//
+
+import SwiftUI
+
+@main
+struct MarsRoverPicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
