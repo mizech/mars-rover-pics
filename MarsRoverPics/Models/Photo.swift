793361ae12d8836b585b450ad55d43de48037d44
mizech <michael.zech@hotmail.de>
2024-06-22 10:45:32 +0200

    Impl.: Fetching Images

---
+++ MarsRoverPics/Models/Photo.swift
@@ -0,0 +1,8 @@
+//
+//  Photo.swift
+//  MarsRoverPics
+//
+//  Created by Michael on 22.06.24.
+//
+
+import Foundation
---
+++ MarsRoverPics/Models/Photos.swift
@@ -0,0 +1,8 @@
+//
+//  Photos.swift
+//  MarsRoverPics
+//
+//  Created by Michael on 22.06.24.
+//
+
+import Foundation
