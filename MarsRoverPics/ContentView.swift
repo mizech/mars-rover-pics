1fd929acd35bc58153c7356fbde9d845a55146bd
mizech <michael.zech@hotmail.de>
2024-06-24 16:43:00 +0200

    Display the loaded Photos in a ForEach.

--- MarsRoverPics/ContentView.swift
+++ MarsRoverPics/ContentView.swift
@@ -4,17 +4,23 @@ struct ContentView: View {
     @Environment(ContentViewModel.self) var contentVM
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 25) {
+            Button("Load Pictures") {
+                Task {
+                    await contentVM.loadPhotos()
+                }
+            }
+            List {
+                ForEach(contentVM.photos) { photo in
+                    Section {
+                        AsyncImage(url: URL(string: photo.img_src))
+                            .frame(width: 200)
+                        Text("Taken on: \(photo.earth_date)")
+                    }
+                }
+            }.listStyle(.plain)
         }
         .padding()
-        .task {
-            await contentVM.loadPhotos()
-            print(contentVM.photos)
-        }
     }
 }
 
